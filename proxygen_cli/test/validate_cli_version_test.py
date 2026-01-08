5f9e79ad7651a7ed04864c39d990e0cc6fae30f8
raghuram grandhi <raghu.ramgrandhi1@nhs.net>
2026-01-08 14:04:55 +0000

    updated required version.

--- proxygen_cli/test/validate_cli_version_test.py
+++ proxygen_cli/test/validate_cli_version_test.py
@@ -13,13 +13,13 @@ def test_validate_cli_version_success(patch_request):
 
 
 def test_validate_cli_version_failure(patch_request):
-    required_version = "3.0.0"
+    required_version = "4.0.0"
 
     mocked_response = {"proxygen_cli": {"min_version": required_version}}
     with patch_request(200, mocked_response), pytest.raises(RuntimeError) as e:
         validate_cli_version()
 
-    assert (
+        assert (
             str(e.value)
-            == "This version proxygen-cli is out-of-date. Please update to 3.0.0"
+            == "This version proxygen-cli is out-of-date. Please update to 4.0.0"
         )
